5eb25118f779bf3a3355ea01bf85a3cbdaf9980a
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-22 23:12:45 +0000

    something

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ var complete = ["eat", "sleep"];
 
 app.get('/', function(req, res)
 {
+    Todo.find(function(err,todo){
+        if(err){
+            console.log(err);
+        }
+        else{
+            task = [];
+            for(i = 0; i < todo.length; i++){
+                task.push(todo[i].item);
+            }
+        }
+    })
     res.render("index", {task:task, complete:complete});
 });
 
