29eaf792c29fc1968a2baf1ab823e24955e7cd9c
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-22 22:54:55 +0000

    mongo

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ var path = require('path');
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ encoded: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false}));
+const Todo = require('./models/todo.model');
+const mongoDB = 'mongodb+srv://Boom:eKIHuVFhrqKuAzOW@cluster0-gahtk.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
 
 var task = ["bark", "meow"];
 var complete = ["eat", "sleep"];
@@ -20,9 +28,16 @@ app.get('/', function(req, res)
 
 app.post('/addtask', function(req, res)
 {
-    var newTask = req.body.newtask;
-    task.push(newTask);
-    res.redirect('/');
+    let newTodo = new Todo({
+        item: req.body.newtask,
+        done: false
+    })
+    newTodo.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 app.post('/removetask', function(req, res)
