7e4c696c6a24e377f2ab9145a7494faedb49c023
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-11-07 23:32:41 +0000

    er

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,18 @@ import './App.css';
 class App extends Component {
     render(){
         const App = () => (
-
             <div>
                 <Switch>
                     <Route exact path='/' component={Home} />
                 </Switch>
             </div>
-        );
-
+        )
         return (
             <Switch>
                 <App />
             </Switch>
-    );
-    }
+        );
+  }
 }
 
 export default App;
